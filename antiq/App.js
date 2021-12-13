ac450de7caf76ec0a877b2bebf566a3405c6bc1c
kwieczorek-cdv <kwieczorek@edu.cdv.pl>
2021-12-13 20:07:42 +0100

    Input.js

--- antiq/App.js
+++ antiq/App.js
@@ -4,8 +4,6 @@ import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 //import fonts from './src/theme/fonts'
 import Container from './src/containers/Container';
 import PrimaryButton from './src/components/PrimaryButton';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import SecondaryButton from './src/components/SecondaryButton';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -21,22 +19,9 @@ export default function App() {
         </Container>
       </SafeAreaView>
     </NavigationContainer>
-=======
-=======
->>>>>>> parent of df7492a (Primary & Secondary button styles)
+  )
+};
 
-export default function App() {
-  return (
-    <SafeAreaView>
-      <Container>
-          <Text>Welcome! This is antiq project.</Text>
-          <StatusBar style="auto" />
-          <PrimaryButton text='PrimaryButton'/>
-      </Container>
-    </SafeAreaView>
->>>>>>> parent of df7492a (Primary & Secondary button styles)
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
