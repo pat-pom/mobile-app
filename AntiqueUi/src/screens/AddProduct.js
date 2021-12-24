5263a8488c956baac77dd978561fddfefe1a2684
rluczak <rluczak@egnyte.com>
2021-12-25 00:08:46 +0100

    Addded action sheet

--- AntiqueUi/src/screens/AddProduct.js
+++ AntiqueUi/src/screens/AddProduct.js
@@ -1,15 +1,32 @@
-import { StyleSheet, ScrollView, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, Image, TouchableOpacity, Button } from 'react-native';
+import { ActionSheet } from 'react-native-cross-actionsheet';
 
 import AddPhotoPlaceholder from "../assets/images/AddPhotoPlaceholder.png";
 
 export const AddProduct = () => {
+  const openActionSheet = () =>
+    ActionSheet.showActionSheetWithOptions(
+      {
+        options: ["Cancel", "Choose from library", "Take a picture"],
+        cancelButtonIndex: 0,
+        userInterfaceStyle: 'light'
+      },
+      buttonIndex => {
+        if (buttonIndex === 1) {
+          alert(buttonIndex)
+        } else if (buttonIndex === 2) {
+          alert(buttonIndex)
+        }
+      }
+    );
+
   return (
     <ScrollView>
       <View style={styles.container}>
-        <TouchableOpacity style={styles.imageContainer} onPress={() => alert("Works")}>
+        <TouchableOpacity style={styles.imageContainer} onPress={openActionSheet}>
           <Image style={styles.image} source={AddPhotoPlaceholder} />
           <Text style={styles.placeholder}>
-            Upload JPG, JPEG, PNG or HEIC up to 10MB
+            Prześlij JPG, JPEG, PNG or HEIC do 10MB
           </Text>
         </TouchableOpacity>
       </View>
