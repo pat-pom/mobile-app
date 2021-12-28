bbd1d2654ed61003342eff1ae215ae9f54ff13f9
kwieczorek-cdv <kwieczorek@edu.cdv.pl>
2021-12-28 22:01:37 +0100

    Search view

--- AntiqueUi/src/screens/Search.js
+++ AntiqueUi/src/screens/Search.js
@@ -1,15 +1,40 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { BottomNavigation } from '../components/BottomNavigation';
+
+import { Dimensions } from 'react-native';
+const {width, height} = Dimensions.get('window');
+
+const metrics = {
+  screenWidth: width < height ? width : height,
+  screenHeight: width < height ? height : width,
+}
 export const Search = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Search Screen</Text>
+      <TextInput
+        style={{
+          height: 52,
+          borderColor: '#969BAB',
+          borderWidth: 1,
+          borderRadius: 4,
+          paddingLeft: 16,
+          fontSize: 14,
+          //lineHeight: 28,
+          fontWeight: "400",
+          color:'#969BAB',
+          marginTop: 32,
+          marginBottom: 40,
+        }}
+        placeholder="Search"
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
+    container: {
+        height: metrics.screenHeight,
+        marginLeft: 24,
+        marginRight: 24,
+      },
 });
