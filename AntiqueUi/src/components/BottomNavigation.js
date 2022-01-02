0b63891c73a5e269cfafd57ce42f9dd2e535d341
pat-pom <ppomierski@edu.cdv.pl>
2022-01-02 19:41:08 +0100

    added Login screen

--- AntiqueUi/src/components/BottomNavigation.js
+++ AntiqueUi/src/components/BottomNavigation.js
@@ -6,6 +6,7 @@ import { Splash } from '../screens/Splash';
 import { Search } from '../screens/Search';
 import { Chat } from '../screens/Chat';
 import { Join } from '../screens/Join';
+import { Login } from '../screens/Login';
 import { Camera } from 'react-feather';
 import { StyleSheet } from 'react-native';
 
@@ -25,6 +26,7 @@ export function BottomNavigation() {
       {/* <Tab.Screen name="Search" component={Search} /> */}
       <Tab.Screen name="Splash" component={Splash} />
       <Tab.Screen name="Join" component={Join} />
+      <Tab.Screen name="Login" component={Login} />
       <Tab.Screen name="AddProduct" component={AddProduct}  style={styles.callToAction}  />
       <Tab.Screen name="Chat" component={Chat} options={{ tabBarBadge: 3 }}/>
       <Tab.Screen name="Profile" component={Profile} />
