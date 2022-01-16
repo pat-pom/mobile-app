381789d1dd31f1f0fd3797edb37abf97f1a59504
rluczak <rluczak@edu.cdv.pl>
2022-01-16 15:51:03 +0100

    Add base crud operation for Product

--- AntiqueApi/Controllers/ProductController.cs
+++ AntiqueApi/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AntiqueApi.Data;
+using AntiqueApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AntiqueApi.Controllers
 {
@@ -6,6 +9,73 @@ namespace AntiqueApi.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly ApiDbContext _context;
 
+        public ProductController(ApiDbContext apiDbContext)
+        {
+            _context = apiDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(ProductData productData)
+        {
+            if(ModelState.IsValid)
+            {
+                await _context.Products.AddAsync(productData);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetProduct", new { productData.Id }, productData);
+            }
+
+            return new JsonResult("Something went wrong :(") { StatusCode = 500 };
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(Guid id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product == null) { return NotFound(); }
+
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(Guid id, ProductData productData)
+        {
+            if(id != productData.Id) { return BadRequest(); }
+
+            var existProduct = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existProduct == null) { return NotFound(); }
+
+            existProduct.Title = productData.Title;
+            existProduct.Description = productData.Description;
+            
+            await _context.SaveChangesAsync();
+
+            return Ok(existProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+            var existProduct = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existProduct == null) { return NotFound(); }
+
+            _context.Products.Remove(existProduct);
+            await _context.SaveChangesAsync();
+
+            return Ok(existProduct);
+        }
     }
 }
