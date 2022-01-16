00cfc4800b63ad7364895b2a7b20ad40c2c63739
rluczak <rluczak@edu.cdv.pl>
2022-01-16 14:29:11 +0100

    Connect API with Azure database

---
+++ AntiqueApi/Data/ApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AntiqueApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AntiqueApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public virtual DbSet<ProductData>? Products { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+    }
+}
