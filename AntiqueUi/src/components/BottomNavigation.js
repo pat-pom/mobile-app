f8c7674a2755415087d8998e396dfef3fe0031de
kwieczorek-cdv <kwieczorek@edu.cdv.pl>
2022-01-16 11:06:09 +0100

    Update BottomNavigation.js

--- AntiqueUi/src/components/BottomNavigation.js
+++ AntiqueUi/src/components/BottomNavigation.js
@@ -60,7 +60,7 @@ export function BottomNavigation() {
       <Tab.Screen name="Home" component={Home} />
       {/* <Tab.Screen name="Notifications" component={Notifications} options={{ headerShown: true, tabBarShowLabel: false, title: "Powiadomienia" }} /> */}
       {/* <Tab.Screen name="ForgotPassword" component={ForgotPassword} options={{headerShown: true, tabBarShowLabel: false, title: "Odzyskaj hasło"}}/> */}
-      <Tab.Screen name="Checkout" component={Checkout} />
+      <Tab.Screen name="Search" component={Search} />
       {/* <Tab.Screen name="Splash" component={Splash} /> */}
       {/* <Tab.Screen name="Join" component={Join} />
       <Tab.Screen name="Login" component={Login} />
