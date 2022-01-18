cfab265080d4df6f7aee677e1b3f9791b86864fe
rluczak <rluczak@egnyte.com>
2022-01-18 21:10:21 +0100

    implement base crud operations

--- backend/src/modules/products/products.controller.ts
+++ backend/src/modules/products/products.controller.ts
@@ -1,18 +1,11 @@
 import * as express from "express";
 import { Product } from "./product.interface";
+import { productModel } from "./product.model";
 
 export class ProductController {
   public path = "/products";
   public router = express.Router();
-
-  private products: Product[] = [
-    {
-      title: "Krzesło",
-      description: "Krzesło na którym siedział Batory",
-      price: 120,
-      state: "uzywane",
-    },
-  ];
+  private product = productModel;
 
   constructor() {
     this.initializeRoutes();
@@ -20,16 +13,51 @@ export class ProductController {
 
   public initializeRoutes() {
     this.router.get(this.path, this.getAllProducts);
+    this.router.get(`${this.path}/:id`, this.getByProductId);
     this.router.post(this.path, this.createProduct);
+    this.router.patch(`${this.path}/:id`, this.updateProduct);
+    this.router.delete(`${this.path}/:id`, this.deleteProduct);
   }
 
   getAllProducts = (request: express.Request, response: express.Response) => {
-    response.send(this.products);
+    this.product.find().then((products) => {
+      response.send(products);
+    });
+  };
+
+  getByProductId = (request: express.Request, response: express.Response) => {
+    const id = request.params.id;
+    this.product.findById(id).then((product) => {
+      response.send(product);
+    });
   };
 
   createProduct = (request: express.Request, response: express.Response) => {
     const product: Product = request.body;
-    this.products.push(product);
-    response.send(product);
+    const createdProduct = new this.product(product);
+    createdProduct.save().then((savedProduct) => {
+      response.send(savedProduct);
+    });
+  };
+
+  updateProduct = (request: express.Request, response: express.Response) => {
+    const id = request.params.id;
+    const productData: Product = request.body;
+    this.product
+      .findByIdAndUpdate(id, productData, { new: true })
+      .then((product) => {
+        response.send(product);
+      });
+  };
+
+  deleteProduct = (request: express.Request, response: express.Response) => {
+    const id = request.params.id;
+    this.product.findByIdAndDelete(id).then((successResponse) => {
+      if (successResponse) {
+        response.sendStatus(200);
+      } else {
+        response.sendStatus(404);
+      }
+    });
   };
 }
